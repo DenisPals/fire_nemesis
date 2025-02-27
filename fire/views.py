8c3b805b609d242ab4149d2a89336297e064041a
Denis <palsdenisworking@yahoo.com>
2025-02-27 14:06:42 +0000

    phone_number_added

--- fire/views.py
+++ fire/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+PHONE_NUMBER = '0330 223 7058'
+
 # Create your views here.
 def index(request):
-    return render(request, 'fire/index.html')
+    return render(request, 'fire/index.html', {'PHONE_NUMBER': PHONE_NUMBER})
